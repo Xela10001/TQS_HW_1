7541d750634d7efea9e3252f613cf4c3200cb482
Xela10001 <alexandre.gazur@ua.pt>
2023-04-08 11:13:49 +0100

    Created springboot application

---
+++ src/main/java/alexandregazur/tqs_hw_1/TqsHw1Application.java
@@ -0,0 +1,13 @@
+package alexandregazur.tqs_hw_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsHw1Application.class, args);
+	}
+
+}
